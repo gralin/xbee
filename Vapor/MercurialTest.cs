95276879be1207633623c4dfb55de0a46208a13d
ransomhall <ransomhall@gmail.com>
2012-02-07 10:44:09 -0500

    Placeholder code (VaporWare) to get this project moving!

---
+++ Vapor/MercurialTest.cs
@@ -0,0 +1,35 @@
+﻿// ********************************************************************************
+//     Document    :  MercurialTest.cs 
+//     Author      :  Eric Hall
+//     License     :  TBD
+//     Description :  Exact format of file header comment block is TBD.
+// ********************************************************************************
+
+namespace Gadgeteer.Modules.Codeplex.XBee.Vapor
+{
+    /// <summary>
+    /// Source Control Test object.
+    /// </summary>
+    /// <remarks>
+    /// XML comments are required! Details TBD.
+    /// </remarks>
+    public class MercurialTest
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        static MercurialTest()
+        {
+            // Lots of inline comments are nice to have.
+            Hello = "Hello from Gadgeteer.Modules.Codeplex.XBee.Vapor.MercurialTest";
+        }
+
+        /// <summary>
+        /// A static string to see if your local Mercurial source control is behaving.
+        /// </summary>
+        public static string Hello
+        {
+            get; private set;
+        }
+    }
+}
---
+++ Vapor/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("VaporWare Mercurial Test")]
+[assembly: AssemblyDescription("Mercurial source control test.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VaporWare")]
+[assembly: AssemblyProduct("Gadgeteer.Modules.Codeplex.XBee.Vapor")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
