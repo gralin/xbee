9b2958bcef1fcdeffd47947aacc3f17e1fa69d9c
Jakub Bartkowiak <gralinpl@gmail.com>
2012-02-08 01:21:09 +0100

    added gadgeteer module source code template

---
+++ source/NETMF Tester/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+
+namespace NETMF.Tester
+{
+    public class Program
+    {
+        private Gadgeteer.Modules.GHIElectronics.Api.XBee _xbee;
+
+        public static void Main()
+        {
+            Debug.Print("Program Started");
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
