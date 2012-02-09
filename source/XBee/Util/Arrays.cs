386996f5659366a73020bb81d93225592ce31950
Jakub Bartkowiak <gralinpl@gmail.com>
2012-02-09 13:48:13 +0100

    Added ported heper classes In IntArrayOutputStream instead of List i used MemoryStream (not tested) IIntArray interface needs to be changed from to use byte instead of int (my opinion)

--- source/XBee/Util/Arrays.cs
+++ source/XBee/Util/Arrays.cs
@@ -1,10 +1,15 @@
 using System;
 
-namespace Gadgeteer.Modules.GHIElectronics.Api
+namespace Gadgeteer.Modules.GHIElectronics.Util
 {
     public static class Arrays
     {
         public static bool AreEqual(int[] array1, int[] array2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int HashCode(int[] array)
         {
             throw new NotImplementedException();
         }
---
+++ source/XBee/Util/IntArrayOutputStream.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+
+namespace Gadgeteer.Modules.GHIElectronics.Util
+{
+    public class IntArrayOutputStream : IIntArray
+    {
+        private readonly MemoryStream _stream;
+
+        public IntArrayOutputStream()
+        {
+            _stream = new MemoryStream();
+        }
+
+        public void Write(byte val)
+        {
+            _stream.WriteByte(val);
+        }
+
+        public void Write(int val)
+        {
+            _stream.WriteByte((byte)val);
+        }
+
+        public void Write(byte[] val)
+        {
+            _stream.Write(val, 0, val.Length);
+        }
+
+        public void Write(int[] val)
+        {
+            foreach (var i in val)
+                _stream.WriteByte((byte)i);
+        }
+
+        public int[] GetIntArray()
+        {
+            _stream.Position = 0;
+            var result = new byte[_stream.Length];
+            _stream.Read(result, 0, result.Length);
+            var resultAsIntArray = new int[result.Length];
+
+            for (var i = 0; i < result.Length; i++)
+                resultAsIntArray[i] = result[i];
+
+            return resultAsIntArray;
+        }
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+        }
+    }
+}
