4e9c6cf38506b3b01d9ff4d74848f90e17f52f20
Jakub Bartkowiak <gralinpl@gmail.com>
2012-02-14 09:17:41 +0100

    Backed out changeset: 816748e47795 Build break

--- source/XBee.Core/Util/Arrays.cs
+++ source/XBee.Core/Util/Arrays.cs
@@ -4,6 +4,16 @@ namespace Gadgeteer.Modules.GHIElectronics.Util
 {
     public static class Arrays
     {
+        public static bool AreEqual(int[] array1, int[] array2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int HashCode(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+
         public static byte[] ToByteArray(int[] array)
         {
             var result = new byte[array.Length];
