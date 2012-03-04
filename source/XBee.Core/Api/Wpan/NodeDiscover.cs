0d2e3696e4b5d2ba081a89ed0fa8da134ab5ddbd
Jakub Bartkowiak <gralinpl@gmail.com>
2012-03-04 14:31:28 +0100

    Restored files with upper camel case names

---
+++ source/XBee.Core/Api/Wpan/NodeDiscover.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Gadgeteer.Modules.GHIElectronics.Api.At;
+using Gadgeteer.Modules.GHIElectronics.Util;
+
+namespace Gadgeteer.Modules.GHIElectronics.Api.Wpan
+{
+    /// <summary>
+    /// Series 1 XBee.
+    /// Parses a Node Discover (ND) AT Command Response
+    /// </summary>
+    public class NodeDiscover : NodeInfo
+    {
+        public int Rssi { get; set; }
+
+        public static NodeDiscover Parse(XBeeResponse response)
+        {
+            return Parse(response as AtResponse);
+        }
+
+        public static NodeDiscover Parse(AtResponse response)
+        {
+            if (response.Command != AtCmd.NodeDiscover)
+                throw new ArgumentException("This method is only applicable for the ND command");
+
+            // empty response is received after the last disovered node
+            // this happens only with Wpan nodes, not Zigbee
+            if (response.Value == null || response.Value.Length == 0)
+                return null;
+
+            var input = new InputStream(response.Value);
+
+            var frame = new NodeDiscover
+            {
+                NetworkAddress = new XBeeAddress16(input.Read(2)),
+                SerialNumber = new XBeeAddress64(input.Read(8)),
+                Rssi = -1*input.Read()
+            };
+
+            byte ch;
+
+            // NI is terminated with 0
+            while ((ch = input.Read()) != 0)
+                if (ch > 32 && ch < 126)
+                    frame.NodeIdentifier += (char)ch;
+
+            return frame;
+        }
+
+        public override string ToString()
+        {
+            return "networkAddress=" + NetworkAddress
+                   + ",serialNumber=" + SerialNumber
+                   + ",nodeIdentifier=" + NodeIdentifier
+                   + ",rssi=" + Rssi + "dBi";
+        }
+    }
+}
---
+++ source/XBee.Core/Api/Zigbee/ForceSampleRequest.cs
@@ -0,0 +1,19 @@
+﻿using Gadgeteer.Modules.GHIElectronics.Api.At;
+
+namespace Gadgeteer.Modules.GHIElectronics.Api.Zigbee
+{
+    /// <summary>
+    /// Uses Remote AT to send a Force Sample (IS) AT command to a remote XBee
+    /// </summary>
+    public class ForceSampleRequest : RemoteAtCommand
+    {
+        /// <summary>
+        /// Creates a Force Sample Remote AT request
+        /// </summary>
+        /// <param name="destination"></param>
+        public ForceSampleRequest(XBeeAddress destination)
+            : base(AtCmd.ForceSample, destination, null, false)
+        {
+        }
+    }
+}
