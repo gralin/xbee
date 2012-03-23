31c90c27532b6eebb7f19a0d4cbacdbd63f5c31b
Jakub Bartkowiak <gralinpl@gmail.com>
2012-03-23 14:14:07 +0100

    Updated ProfileId enum

--- source/XBee/Api/Zigbee/ProfileId.cs
+++ source/XBee/Api/Zigbee/ProfileId.cs
@@ -1,11 +1,34 @@
 ﻿namespace NETMF.OpenSource.XBee.Api.Zigbee
 {
+    /// <summary>
+    /// Application profiles provide messaging schemes allowing to communicate with connected device.
+    /// </summary>
     public enum ProfileId : ushort
     {
         /// <summary>
-        /// Zigbee Device Profile.
+        /// Zigbee Device Profile (ZDP).
         /// </summary>
-        ZDP = 0,
+        ZigbeeDevice = 0x0000,
+
+        /// <summary>
+        /// Smart homes (HA).
+        /// </summary>
+        HomeAutomation = 0x0104,
+
+        /// <summary>
+        /// Value-added services (TA).
+        /// </summary>
+        TelecomServices = 0x0107,
+        
+        /// <summary>
+        /// Health and fitness monitoring (HC).
+        /// </summary>
+        HealthCare = 0x0108,
+
+        /// <summary>
+        /// Home energy savings (SE). 
+        /// </summary>
+        SmartEnergy = 0x0109,
 
         /// <summary>
         /// Digi private profile called Drop-In-Networking.
